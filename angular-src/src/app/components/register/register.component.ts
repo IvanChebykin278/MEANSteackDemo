5e8a36d578385bf5c4fd4bff36cfdeeb94dbbaaf
Ponomarenko <sergey.ponomarenko@sap.com>
2019-07-11 14:14:37 +0300

    sixth commit

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import { AuthService } from 'app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +14,45 @@ export class RegisterComponent implements OnInit {
   username: String;
   password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, 
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log(123);
+    //console.log(123);
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    //Required Fields
+    if(!this.validateService.validateRegister(user)) {
+      console.log('zapolni vse');
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)) {
+      console.log('email ne validen');
+      return false;
+    }
+
+    //Register user
+    this.authService.registerUser(user).subscribe(data => {
+        console.log(data);
+        if(data.success) {
+          console.log("vse ok!");
+          this.router.navigate(['/login']);
+        } else {
+          console.log("takoi uje est");
+          this.router.navigate(['/register']);
+        }
+    });
   }
+
 
 }
