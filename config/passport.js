1f4a14ddada864288e498d66aa5c62e40e056a6a
Ponomarenko <sergey.ponomarenko@sap.com>
2019-07-11 09:47:45 +0300

    second commit

--- config/passport.js
+++ config/passport.js
@@ -7,18 +7,18 @@ module.exports = function(passport) {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme ("jwt");
     opts.secretOrKey = config.secret;
-    passport.use(new JwtStrategy(opts, function(jwt_payload) {
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         console.log(jwt_payload);
         User.getUserById(jwt_payload._id, function(err, user){
-            // if(err) {
-            //     return done(err, false);
-            // }
+            if(err) {
+                return done(err, false);
+            }
 
-            // if(user) {
-            //     return done(null, user);
-            // } else {
-            //     return done(null, false);
-            // }
+            if(user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
         });
     }));
 }
